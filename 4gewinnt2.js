84a87d0be7ef961ff7eac3b708ab382b37c783a8
David Decker <david.decker@apptiva.ch>
2019-10-24 10:19:44 +0200

    backend

--- 4gewinnt2.js
+++ 4gewinnt2.js
@@ -1,19 +1,10 @@
-let table = [[0,0,0,0,0,0,0],
-			[0,0,0,0,0,0,0],
-			[0,2,0,0,0,0,0],
-			[0,0,2,0,0,0,0],
-			[0,0,0,2,0,0,0],
-			[0,0,0,0,2,0,0]]
-
-let playerNr = 1
-let winMark = 0
-
+let gameState = newGame()
 
 
 function dropAChip(x){
 	for(let y=5;y>=0;y--){
-		if(table[y][x] == 0){
-			table[y][x] = playerNr
+		if(gameState.table[y][x] == 0){
+			gameState.table[y][x] = playerNr
 			if(playerNr == 1){
 				playerNr = 2
 			}else{
@@ -24,80 +15,72 @@ function dropAChip(x){
 	}
 }
 
-function winCheckHorizontal(player){
-	for(let y=table.length-1;y>=0;y--){
-		for(let x=0;x<table[0].length;x++){
-			if(table[y][x] == player){
-				winMark++
-			}else{
-				winMark = 0
-			}
-			if(winMark == 4){
-				return true
-			}
+function horizontal(y,x,player){
+	for(let d=0;d<4;d++){
+		if(!gameState.table[y-d] || gameState.table[y][x+d] !== player){
+			return false
 		}
 	}
+	return true
 }
 
-function winCheckVertical(player){
-	for(let x=0;x<table[0].length;x++){
-		for(let y=table.length-1;y>=0;y--){
-			if(table[y][x] == player){
-				winMark++
-			}else{
-				winMark = 0
-			}
-			if(winMark == 4){
-				return true
-			}
+function vertical(y,x,player){
+	for(let d=0;d<4;d++){
+		if(!gameState.table[y-d] || gameState.table[y-d][x] !== player){
+			return false
 		}
 	}
+	return true
 }
 
 function diagonalRight(y,x,player){
 	for(let d=0;d<4;d++){
-		if(table[y-d] && table[y-d][x+d] == player){
-			winMark++
-		}else{
-			winMark = 0
-		}
-		if(winMark == 4){
-			return true
+		if(!gameState.table[y-d] || gameState.table[y-d][x+d] !== player){
+			return false
 		}
 	}
+	return true
 }
 function diagonalLeft(y,x,player){
 	for(let d=0;d<4;d++){
-		if(table[y-d] && table[y-d][x-d] == player){
-			winMark++
-		}else{
-			winMark = 0
-		}
-		if(winMark == 4){
-			return true
+		if(!gameState.table[y-d] || gameState.table[y-d][x-d] !== player){
+			return false
 		}
 	}
+	return true
 }
 
-function winCheckDiagonal(player){
-	for(let y=table.length-1;y>=0;y--){
-		for(let x=0;x<table[0].length;x++){
+function winCheck(player){
+	for(let y=gameState.table.length-1;y>=0;y--){
+		for(let x=0;x<gameState.table[0].length;x++){
 			if(diagonalRight(y,x,player)
-			|| diagonalLeft(y,x,player)){
+			|| diagonalLeft(y,x,player)
+			|| horizontal(y,x,player)
+			|| vertical(y,x,player)){
 				return true
 			}
 		}
 	}
 }
 
-
-function winCheck(nr){
-	return winCheckHorizontal(nr) ||
-	winCheckVertical(nr) ||
-	winCheckDiagonal(nr)
+function winner(){
+	for(let i=1;i<3;i++){
+		if(winCheck(i)){
+			console.log(`Player ${i} won!`)
+		}
+	}
 }
 
-winCheck(1)
-winCheck(2)
+function newGame(){
+	return {
+		table: [[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0]],
+		playerNr: 1,
+	}
+}
 
-console.log(table)
+console.log(gameState.table)
