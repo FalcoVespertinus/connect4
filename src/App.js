f7dc4c4cdcffc6be07e10f75a517c0bd673a1f10
David Decker <daviddecker@Davids-MacBook-Pro.local>
2019-10-29 15:51:56 +0100

    react

--- src/App.js
+++ src/App.js
@@ -7,29 +7,38 @@ class App extends React.Component {
     this.state = {gameState: newGame()}
   }
   render(){
+    let tableColor
+    if(this.state.gameState.winner !== 0){
+      tableColor = "black"
+    }
     return (
-      <div className="board">
-        <table className="table">
-          <tbody>
-            {this.state.gameState.table.map(zeile=>{
-              return <tr>
-                {zeile.map((zelle,column)=>{
-                  let color
-                  if(zelle == "1"){
-                    color = "red"
-                  }
-                  if(zelle == "2"){
-                    color = "yellow"
-                  }
-                  return <td className="column" style={{backgroundColor: color}} onClick={()=>{
-                    dropAChip(column,this.state.gameState)
-                    this.setState({})
-                  }}></td>
-                })}
-              </tr>
-            })}
-          </tbody>
-        </table>
+      <div>
+        <div className="board" style={{backgroundColor:tableColor,borderColor:tableColor}}>
+          <table className="table">
+            <tbody>
+              {this.state.gameState.table.map(zeile=>{
+                return <tr>
+                  {zeile.map((zelle,column)=>{
+                    let color
+                    if(zelle == "1"){
+                      color = "red"
+                    }
+                    if(zelle == "2"){
+                      color = "yellow"
+                    }
+                    return <td className="column" style={{backgroundColor: color}} onClick={()=>{
+                      if(this.state.gameState.winner == 0){
+                        dropAChip(column,this.state.gameState)
+                        this.setState({})
+                      }
+                    }}></td>
+                  })}
+                </tr>
+              })}
+            </tbody>
+          </table>
+        </div>
+        <button onClick={()=>{this.setState({gameState:newGame()})}} className="play-again">Play Again!</button>
       </div>
     );
   }
